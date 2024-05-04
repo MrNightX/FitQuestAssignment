fc031cf2f0c3c84447c06e9509e7df2534d2c5c8
NightX <NightX@DESKTOP-L7A9PGQ>
2024-05-04 12:55:36 +0800

    Bryan Update: 1. Clean up the backend of Li Wei's code
    Bryan Dev Log:
    "Need to change how it navigates in Li Wei's code, preferably change them to use nav functions"

--- app/src/main/java/com/example/fitquest/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/fitquest/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.fitquest.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.fitquest.R
+import com.example.fitquest.Verification_FIrst
 import com.example.fitquest.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -35,6 +40,15 @@ class HomeFragment : Fragment() {
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController()
+        binding.testButtonGetInfo.setOnClickListener {
+            navController.navigate(R.id.getInfoFirst)
+        }
+
+
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
