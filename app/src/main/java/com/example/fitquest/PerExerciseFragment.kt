94f8368ad93767b72ac61aa8bb84aa5ec28f59c4
NightX <NightX@DESKTOP-L7A9PGQ>
2024-05-17 00:57:06 +0800

    Bryan Update: 1. Per Exercise page is updated This fragment can get image from cloud with a few set ups and commands
    To do:
    Setting up 2 different options
    Opt 1: Transfer data from previous fragment to this fragment
    Opt 2: Get data from real time database

--- app/src/main/java/com/example/fitquest/PerExerciseFragment.kt
+++ app/src/main/java/com/example/fitquest/PerExerciseFragment.kt
@@ -1,25 +1,39 @@
 package com.example.fitquest
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.fitquest.databinding.FragmentPerExerciseBinding
+import com.example.fitquest.ui.UserInfo.Verification_Second
+import com.google.firebase.storage.FirebaseStorage
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PerExerciseFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PerExerciseFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
+
+
+
+    //private lateinit var mUserViewModel : UserViewModel
+    private var _binding : FragmentPerExerciseBinding? = null
+    private val binding get() = _binding!!
+
+    private var ExerciseName: String    = ""
+    private var ExerciseImage : Int     = 0
+    private var ExerciseType: String    = ""
+    private var TargetBody: String      = ""
+    private var CalorieBurned : Int     = 0
+    private var ExerciseInfo : String   = ""
+
+    private var imagePath: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +41,7 @@ class PerExerciseFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -34,8 +49,78 @@ class PerExerciseFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_per_exercise, container, false)
+        _binding = FragmentPerExerciseBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val ONE_MEGABYTE : Long = 1024 * 1024
+
+        binding.buttonTestExercise1.setOnClickListener {
+
+
+            ExerciseName = "Push up"
+            //ExerciseImage = R.drawable.better_pushups
+            imagePath = "ExercisePicture/better_pushups.png"
+            ExerciseType = "Strength"
+            TargetBody = "Chest"
+            CalorieBurned = 100
+            ExerciseInfo = "Push-up exercise is a close chain kinetic exercise which improves the " +
+                    "joint proprioception, joint stability and muscle co-activation around the " +
+                    "shoulder joint."
+
+            ChangeImage(imagePath)
+            ChangeData()
+
+
+        }
+
+        binding.buttonTestExercise2.setOnClickListener {
+            ExerciseName = "Squat"
+            //ExerciseImage = R.drawable.better_squat
+            imagePath = "ExercisePicture/better_squat.png"
+
+            ExerciseType = "Strength"
+            TargetBody = "Leg"
+            CalorieBurned = 200
+            ExerciseInfo = "A squat is a strength exercise in which the trainee lowers their hips " +
+                    "from a standing position and then stands back up."
+
+            ChangeImage(imagePath)
+            ChangeData()
+        }
+
+        binding.buttonBackExercise.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        //Refer to VerificationFirst and VerificationSecond
+
+
+    }
+
+    fun ChangeData()
+    {
+        binding.textViewExerciseName.setText(ExerciseName)
+        binding.imageViewCurrentExercise.setImageResource(ExerciseImage)
+        binding.textViewTypeName.setText(ExerciseType)
+        binding.textViewBodyPartName.setText(TargetBody)
+        binding.textViewCalorie.text = String.format("%d Kcal", CalorieBurned)
+        binding.textViewExerciseInfo.setText(ExerciseInfo)
+    }
+
+    fun ChangeImage(ImagePath: String)
+    {
+        val storageRef = FirebaseStorage.getInstance().getReference(ImagePath)
+        storageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener { bytes ->
+            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            binding.imageViewCurrentExercise.setImageBitmap(bitmap)
+        }.addOnFailureListener {
+            Log.e("FirebaseStorage", "Error downloading image")
+        }
+    }
+
 
     companion object {
         /**
@@ -44,16 +129,17 @@ class PerExerciseFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PerExerciseFragment.
+         * @return A new instance of fragment Verification_Second.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PerExerciseFragment().apply {
+            Verification_Second().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
                 }
             }
     }
+
 }
--- app/src/main/java/com/example/fitquest/ui/Workout/Workout_ChooseMode.kt
+++ app/src/main/java/com/example/fitquest/ui/Workout/Workout_ChooseMode.kt
@@ -5,29 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.fitquest.R
+import com.example.fitquest.databinding.FragmentWorkoutChooseModeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Workout_ChooseMode.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Workout_ChooseMode : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentWorkoutChooseModeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +25,17 @@ class Workout_ChooseMode : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout__choose_mode, container, false)
+        _binding = FragmentWorkoutChooseModeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Workout_ChooseMode.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Workout_ChooseMode().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonRecommend.setOnClickListener{
+            findNavController().navigate(R.id.action_workout_ChooseMode_to_perExerciseFragment)
+        }
     }
+
+
 }
